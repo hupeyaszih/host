535a970504e2acb5f8ab739e2dc95627ca5bc3da
hupeyaszih <19hupeyaszih45@gmail.com>
2026-09-22 18:55:02 +0300

    feat(vga): implement color code macros

--- include/kernel/vga.h
+++ include/kernel/vga.h
@@ -4,6 +4,24 @@
 #define VGA_COLUMNS 80
 #define VGA_ROWS 25
 
+#define VGA_COLOR_BLACK   0x00
+#define VGA_COLOR_BLUE    0x01
+#define VGA_COLOR_GREEN   0x02
+#define VGA_COLOR_CYAN    0x03
+#define VGA_COLOR_RED     0x04
+#define VGA_COLOR_MAGENTA 0x05
+#define VGA_COLOR_BROWN   0x06
+
+#define VGA_COLOR_LIGHT_GRAY      0x07
+#define VGA_COLOR_DARK_GRAY       0x08
+#define VGA_COLOR_LIGHT_BLUE      0x09
+#define VGA_COLOR_LIGHT_GREEN     0x0A
+#define VGA_COLOR_LIGHT_CYAN      0x0B
+#define VGA_COLOR_LIGHT_RED       0x0C
+#define VGA_COLOR_LIGHT_MAGENTA   0x0D
+#define VGA_COLOR_LIGHT_YELLOW    0x0E
+#define VGA_COLOR_LIGHT_WHITE     0x0F
+
 void vga_clear_screen();
 void vga_print_char(char ch, unsigned int color, int x, int y);
 void vga_print_string(char *string, unsigned int color, int x, int y);
--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -8,12 +8,12 @@ void kernel_main(void) {
     vga_clear_screen();
 
 
-    vga_print_string("HUPEYASZIH", 0x0A, VGA_COLUMNS/2, VGA_ROWS/2-3);
-    vga_print_string("Designed by Poyraz BAKIRTAS", 0x0A, VGA_COLUMNS/2, VGA_ROWS/2-1);
-    vga_print_string("-HOST-", 0x0A, VGA_COLUMNS/2, VGA_ROWS/2+1);
+    vga_print_string("HUPEYASZIH", VGA_COLOR_CYAN, VGA_COLUMNS/2, VGA_ROWS/2-3);
+    vga_print_string("Designed by Poyraz BAKIRTAS", VGA_COLOR_LIGHT_GRAY, VGA_COLUMNS/2, VGA_ROWS/2-1);
+    vga_print_string("-HOST-", VGA_COLOR_LIGHT_RED, VGA_COLUMNS/2, VGA_ROWS/2+1);
 
 
     char cpu_str[49];
     x86_64_get_cpu_brand(cpu_str);
-    vga_print_string(cpu_str, 0x0A, VGA_COLUMNS/2, VGA_ROWS/2+3);
+    vga_print_string(cpu_str, VGA_COLOR_GREEN, VGA_COLUMNS/2, VGA_ROWS/2+3);
 }
